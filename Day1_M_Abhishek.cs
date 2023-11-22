1a0a25ed5204234f9d03a3aa7e4a2386d1e8b7cf
Abhishek Singh <56843616+abhishek100699@users.noreply.github.com>
2023-11-22 11:16:51 +0530

    Create Day1_M_Abhishek.cs
    Made changes

---
+++ Day1_M_Abhishek.cs
@@ -0,0 +1,64 @@
+review 1 :
+
+int timeInDays
+------------------------------------------------------------------------------------
+review 2:
+var employeeData = db._employeeService.GetEmployees().ToList(); // Get List of employees
+
+-------------------------------------------------------------------------------------
+review 3:
+
+// Avoid Hunngaries Nota
+int counter;
+string cullName;
+DateTime modifiedDate;
+
+------------------------------------------------------------------------------------
+review 4:
+
+public bool IsShopOpen(string day, int amount)
+{
+    // some logic
+}
+
+
+------------------------------------------------------------------------------------
+
+  review 5:
+
+const int DAYS_IN_WEEK = 7;
+const int DAYS_IN_MONTH = 30;
+
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+bool EraseDatabase() {}
+bool RestoreDtabase() {}
+
+class Animal {}
+class Alpaca {}
+
+------------------------------------------------------------------------------------
+
+
+  review 6:
+
+public class Employee
+{
+    public Datetime WorkDate { get; set; } // get set Start Working Date
+    public Datetime Time { get; set; } // get set Modification Time
+
+}
+
+
+  ------------------------------------------------------------------------------------
+
+
+    review 7:
+
+  var employeePhone;
+
+public double CalculateSalary(int workingDays, int workingHours)
+{
+    // some logic
+}
